ac60097855c2ee40a93b7bb5818d226697b31baa
Robin Nørlev Ezra <17013720+giano574@users.noreply.github.com>
2020-02-23 08:17:38 +0100

    Added experience to description_screen.dart

--- lib/screens/description_screen.dart
+++ lib/screens/description_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hackathon/models/experience.dart';
 import 'package:flutter_hackathon/widgets/review_list.dart';
 
 class DescriptionScreen extends StatelessWidget {
-  final String title;
+  final Experience experience;
 
-  const DescriptionScreen({Key key, this.title}) : super(key: key);
+  const DescriptionScreen({Key key, this.experience}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +13,7 @@ class DescriptionScreen extends StatelessWidget {
       length: tabs.length,
       child: Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text(experience.name),
           bottom: TabBar(
             isScrollable: false,
             tabs: tabs.map((ViewTab tab) {
